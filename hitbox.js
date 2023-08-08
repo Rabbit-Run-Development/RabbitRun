18a087a009adc1da54871fb1cf6b08a17dad5647
petthecat19 <adrianpena365@gmail.com>
2023-08-08 20:53:26 +0000

    Added auto-movement for player and enemy

--- hitbox.js
+++ hitbox.js
@@ -1,22 +1,46 @@
-// import kaboom from "kaboom";
+const FLOOR_HEIGHT = 48;
+const JUMP_FORCE = 800;
+const SPEED = 480;
 
-// Initialize kaboom
 kaboom({
     
 });
 
-// Create a box and make it move
-const box = add([
-    rect(80, 80),
-    pos(0, 500),
-    color(139, 0, 0),
-    move(RIGHT, 140), // Note the change here
-]);
+scene("game", () => {
+    
+    setGravity(3000)
 
+    const player = add([
+        rect(80, 80),
+        pos(75, 500),
+        color(255, 182, 193),
+        move(RIGHT, 150),
+    ]);
+    
+    const enemy = add([
+        rect(100, 80),
+        pos(-170, 500),
+        color(255, 109, 10),
+        move(RIGHT, 107)])
+        
+         add([
+        rect(width(), FLOOR_HEIGHT),
+        outline(4),
+        pos(0, height()),
+        anchor("botleft"),
+        area(),
+        body({ isStatic: true }),
+        color(127, 200, 255),
+    ]);
+         
+         function jump() {
+        if (player.isGrounded()) {
+            player.jump(JUMP_FORCE);
+        }
+    }
+        
+        onKeyPress("space", jump);
+        onClick(jump);
+});
 
-
-// setInterval(() => {
-//     marginLeftSquare += 2;
-//     fox.style.marginLeft = `${marginLeftSquare}px`
-
-// },50);
+go("game")
