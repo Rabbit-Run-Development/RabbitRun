809dee71129cacc145dc58dba074e91c49e4c597
petthecat19 <adrianpena365@gmail.com>
2023-08-09 19:59:20 +0000

    Added goal with hitbox

--- hitbox.js
+++ hitbox.js
@@ -1,4 +1,4 @@
-const FLOOR_HEIGHT = 132.5;
+const FLOOR_HEIGHT = 152.5;
 const JUMP_FORCE = 800;
 const SPEED = 300;
 
@@ -9,9 +9,17 @@ kaboom({
 scene("game", () => {
 
     setGravity(2700)
+    
+    const goal = add([
+        rect(60, 60),
+        pos(width() / 1.1, 500),
+        area(),
+        body(),
+        fixed()
+        ])
 
     const player = add([
-        rect(80, 80),
+        rect(65, 65),
         pos(75, 500),
         area(),
         body(),
@@ -20,8 +28,10 @@ scene("game", () => {
     ]);
 
     const enemy = add([
-        rect(100, 80),
-        pos(-170, 500),
+        rect(130, 60),
+        area(),
+        body(),
+        pos(-114, 500),
         color(255, 109, 10),
         move(RIGHT, 42)
     ])
@@ -47,7 +57,7 @@ scene("game", () => {
 
     function spawnRock() {
         add([
-            rect(37, rand(32, 50)),
+            rect(37, rand(35, 35)),
             area(),
             outline(4),
             pos(width(), height() - FLOOR_HEIGHT),
@@ -57,7 +67,7 @@ scene("game", () => {
             "rock",
         ]);
 
-        wait(rand(1, 2), spawnRock);
+        wait(rand(1, 1.5), spawnRock);
 
     }
 
@@ -65,17 +75,17 @@ scene("game", () => {
 
     slowRabbit = () => {
         setTimeout(
-            player.move(-SPEED + 137, 0)
+            player.move(-SPEED + 127, 0)
        )
     }
 
     player.onCollideUpdate("rock", () => {
         slowRabbit();
-        shake();
+        shake(3);
 
     })
     
-    player.onCollide(enemy, () => {
+    player.onCollide("enemy", () => {
         
     })
 
